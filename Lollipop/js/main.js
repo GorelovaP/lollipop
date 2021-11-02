a1fa0e52d59e91ca764adef34128b93e05a8aeb2
GorelovaP <komsomolskai1@mail.ru>
2021-11-02 12:44:46 +0300

    Add pattern

--- Lollipop/js/main.js
+++ Lollipop/js/main.js
@@ -11,7 +11,6 @@ const mySwiper = new Swiper(".swiper-container", {
 //cart
 
 
-
 const buttonCart = document.querySelector(".button-cart");
 const modalCart = document.querySelector("#modal-cart");
 const modalClose = document.querySelector("modal-close");
@@ -247,33 +246,3 @@ showAcsessories.forEach(item => {
         filterCards('category', 'Acsessories');
     })
 })
-
-
-//Телефон
-(function( $ ){
-	
-    var $body;
-  
-    $(document).ready(function(){
-      $body = $('body');
-  
-      $body
-        .find('.user-phone').each(function(){
-            $(this).mask("+375 (44) 999-99-99",{autoclear: false});
-        });
-  
-      $body.on('keyup','.user-phone',function(){
-        var phone = $(this),
-            phoneVal = phone.val(),
-            form = $(this).parents('form');
-  
-        if ( (phoneVal.indexOf("_") != -1) || phoneVal == '' ) {
-          form.find('.btn_submit').attr('disabled',true);
-        } else {
-          form.find('.btn_submit').removeAttr('disabled');
-        }
-      });
-  
-    });
-  
-  })( jQuery );
