b91d319632b8fb155a469275e7d337605dadb80d
Anastasiya31 <burts3va.a@yandex.by>
2021-11-14 22:38:07 +0300

    карточки и главная

--- Lollipop/js/main.js
+++ Lollipop/js/main.js
@@ -212,14 +212,14 @@ for (let i = 0; i < scrollLinks.length; i++) {
 
 const createCard = function ({ label, name, img, id, description, price }) {
   const card = document.createElement("div");
-  card.className = "col-lg-3 col-sm-6";
+  card.className = "col-lg-4 col-sm-6";
   card.innerHTML = `
 	<div class="goods-card">
 	${label ? `<span class="label">${label}</span>` : ""}
 		<img src="db/${img}" alt="${name}" class="goods-image">
 	<h3 class="goods-title">${name}</h3>
      <p class="goods-description">${description}</p>
-	<button class="button goods-card-btn add-to-cart" data-id="${id}">
+	<button class="goods-card-btn add-to-cart" data-id="${id}">
 		<span class="button-price">${price} BYN</span>
 	</button>
 </div>
